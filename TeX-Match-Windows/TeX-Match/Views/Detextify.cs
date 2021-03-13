5ffa6cd15bdbf9f2c739abc4b65044892007dae3
Zoey <me@zoey.fyi>
2021-03-13 10:20:25 -0800

    feat(windows): proof of concept integration with detexify

---
+++ TeX-Match-Windows/TeX-Match/Views/Detextify.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.InteropServices;
+
+namespace TeX_Match.Views
+{
+    unsafe internal class Detextify
+    {
+        [DllImport(@"detexify.dll")]
+        public static extern void* stroke_builder_new(uint capacity);
+
+        [DllImport(@"detexify.dll")]
+        public static extern void stroke_builder_add_point(void* builder, double x, double y);
+
+        [DllImport(@"detexify.dll")]
+        public static extern void* stroke_builder_build(void* builder);
+
+        [DllImport(@"detexify.dll")]
+        public static extern void* stroke_sample_new_builder(uint capacity);
+
+        [DllImport(@"detexify.dll")]
+        public static extern void stroke_sample_add_stroke(void* builder, void* stroke);
+
+        [DllImport(@"detexify.dll")]
+        public static extern void* stroke_sample_build(void* builder);
+
+        [DllImport(@"detexify.dll")]
+        public static extern void* classifier_new_default();
+
+        [DllImport(@"detexify.dll")]
+        public static extern void classifier_free(void* classifier);
+
+        [DllImport(@"detexify.dll")]
+        public static extern void* classify(void* classifier, void* sample);
+
+        [DllImport(@"detexify.dll")]
+        public static extern uint scores_length(void* scores);
+
+        [DllImport(@"detexify.dll")]
+        public static extern string scores_get_command(void* scores, uint i);
+
+        [DllImport(@"detexify.dll")]
+        public static extern string scores_get_package(void* scores, uint i);
+
+        [DllImport(@"detexify.dll")]
+        public static extern string scores_get_font_encoding(void* scores, uint i);
+
+        [DllImport(@"detexify.dll")]
+        public static extern void scores_free(string id);
+
+        [DllImport(@"detexify.dll")]
+        public static extern double scores_get_score(void* scores, uint i);
+    }
+}
--- TeX-Match-Windows/TeX-Match/Views/MainPage.xaml.cs
+++ TeX-Match-Windows/TeX-Match/Views/MainPage.xaml.cs
@@ -1,33 +1,84 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using Windows.Foundation;
+using Windows.UI.Input.Inking;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml.Controls;
 
 namespace TeX_Match.Views
 {
-    public sealed partial class MainPage : Page, INotifyPropertyChanged
+
+    public unsafe sealed partial class MainPage : Page, INotifyPropertyChanged
     {
+        void* classifier;
+
         public MainPage()
         {
             InitializeComponent();
-            DrawingArea.InkPresenter.InputDeviceTypes = Windows.UI.Core.CoreInputDeviceTypes.Mouse | Windows.UI.Core.CoreInputDeviceTypes.Pen;
 
             ApplicationView.PreferredLaunchViewSize = new Size(640, 400);
             ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
             ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(640, 400));
 
-            for (int i =0; i < 100; i++)
-            {
-                ResultsList.Items.Add("Foo" + i);
-            }
+            DrawingArea.InkPresenter.InputDeviceTypes =
+                Windows.UI.Core.CoreInputDeviceTypes.Mouse
+                | Windows.UI.Core.CoreInputDeviceTypes.Touch
+                | Windows.UI.Core.CoreInputDeviceTypes.Pen;
+            DrawingArea.InkPresenter.StrokesCollected += DrawingCanvas_StrokesCollected;
             
+
+            classifier = Detextify.classifier_new_default();
+
+            //void* sampleBuilder = Detextify.stroke_sample_new_builder(0);
+            //void* sample = Detextify.stroke_sample_build(sampleBuilder);
+            //void* scores = Detextify.classify(classifier, sample);
+            //ResultsList.Items.Clear();
+            //for (uint i = 0; i < Detextify.scores_length(scores); i++)
+            //{
+            //    string command = Detextify.scores_get_command(scores, i);
+            //    double score = Detextify.scores_get_score(scores, i);
+            //    ResultsList.Items.Add(String.Format("{0}: command: {1}, score: {2}", i, command, score));
+            //}
+        }
+
+        private void DrawingCanvas_StrokesCollected(InkPresenter sender, InkStrokesCollectedEventArgs args)
+        {
+            System.Collections.Generic.IReadOnlyList<InkStroke> strokes = sender.StrokeContainer.GetStrokes();
+            void* sampleBuilder = Detextify.stroke_sample_new_builder((uint)strokes.Count);
+            
+            foreach (InkStroke stroke in strokes)
+            {
+                System.Collections.Generic.IReadOnlyList<InkPoint> points = stroke.GetInkPoints();
+                void* sb = Detextify.stroke_builder_new((uint)points.Count);
+
+                foreach (InkPoint point in points)
+                {
+                    Detextify.stroke_builder_add_point(sb, point.Position.X, point.Position.Y);
+                }
+
+                void* s = Detextify.stroke_builder_build(sb);
+                Detextify.stroke_sample_add_stroke(sampleBuilder, s);
+            }
+
+            void* sample = Detextify.stroke_sample_build(sampleBuilder);
+            void* scores = Detextify.classify(classifier, sample);
+
+            ResultsList.Items.Clear();
+
+            for (uint i =0; i < Detextify.scores_length(scores); i++)
+            {
+                string command = Detextify.scores_get_command(scores, i);
+                double score = Detextify.scores_get_score(scores, i);
+                ResultsList.Items.Add(String.Format("{0}: command: {1}, score: {2}", i, command, score));
+                //Detextify.scores_free(command);
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        private void Set<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
         {
             if (Equals(storage, value))
             {
@@ -44,5 +95,7 @@ namespace TeX_Match.Views
         {
             DrawingArea.InkPresenter.StrokeContainer.Clear();
         }
+
+
     }
 }
