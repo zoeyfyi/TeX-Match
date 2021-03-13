0fbf64407989bd1576bf29202081614e600b2af4
Zoey <me@zoey.fyi>
2021-03-13 04:58:40 -0800

    feat(windows): basic ui

--- TeX-Match-Windows/TeX-Match/Views/MainPage.xaml.cs
+++ TeX-Match-Windows/TeX-Match/Views/MainPage.xaml.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-
+using Windows.Foundation;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml.Controls;
 
 namespace TeX_Match.Views
@@ -11,6 +12,17 @@ namespace TeX_Match.Views
         public MainPage()
         {
             InitializeComponent();
+            DrawingArea.InkPresenter.InputDeviceTypes = Windows.UI.Core.CoreInputDeviceTypes.Mouse | Windows.UI.Core.CoreInputDeviceTypes.Pen;
+
+            ApplicationView.PreferredLaunchViewSize = new Size(640, 400);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(640, 400));
+
+            for (int i =0; i < 100; i++)
+            {
+                ResultsList.Items.Add("Foo" + i);
+            }
+            
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -27,5 +39,10 @@ namespace TeX_Match.Views
         }
 
         private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private void ClearButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            DrawingArea.InkPresenter.StrokeContainer.Clear();
+        }
     }
 }
