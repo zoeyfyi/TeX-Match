e22636c05511a9b62967f1cee68c1bc28e4437bc
Zoey <me@zoey.fyi>
2021-03-22 11:26:32 -0700

    ci(flatpak): fix submodules folder path

--- TeX-Match-Linux/flatpak/gen-sources.sh
+++ TeX-Match-Linux/flatpak/gen-sources.sh
@@ -4,6 +4,6 @@
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
-python3 $SCRIPTPATH/../submodules/flatpak-builder-tools/cargo/flatpak-cargo-generator.py $SCRIPTPATH/../Cargo.lock
+python3 $SCRIPTPATH/../../submodules/flatpak-builder-tools/cargo/flatpak-cargo-generator.py $SCRIPTPATH/../Cargo.lock
 
 mv $PWD/generated-sources.json $SCRIPTPATH/generated-sources.json
